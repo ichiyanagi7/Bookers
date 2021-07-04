ab27b9bada72c200971608dc63526d28a53b2efd
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-07-04 13:23:23 +0000

    [add]3-8 コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/new'
+  get 'homes/edit'
+  get 'homes/top'
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
